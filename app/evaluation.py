a279df7eb55b4d4933bdbd71663af1dc15a46f45
Lucy Howarth <lhowarth@aqa.org.uk>
2024-10-15 12:39:44 +0100

    Evaluation edit to fix

--- app/evaluation.py
+++ app/evaluation.py
@@ -4,6 +4,7 @@ import os
 import json
 import openai
 
+
 load_dotenv()
 class Params(TypedDict):
     pass
@@ -79,9 +80,4 @@ def evaluation_function(response, answer, parameters):
         output["feedback"] = feedback
 
     return output
-<<<<<<< HEAD
-print("test")
-=======
-    print("test")
 
->>>>>>> a52bf76363fb232fc900e2d33910ac47e9562f86
